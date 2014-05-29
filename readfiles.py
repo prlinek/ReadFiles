cb8fca440b585c7e16db309f342e8394d6e86b06
prlinek <prlinek@gmail.com>
2014-05-29 10:30:44 +0100

    v0.2
    Improved reading values from files. No container for all of the values -
    accessing value from chosen file not possible yet.

--- readfiles.py
+++ readfiles.py
@@ -6,18 +6,20 @@ import glob
 import os
 #import sys
 
-datapath = './DataPack1'
+datapath = '../DataPack1'
 filemask = 'scan_down_'
 
 if not os.path.isdir(datapath):
     print "Entered Data path does not exist!"
 
-rows = []
+# rows = []
 x = []
-# y = numpy.array([])
+
 
 # reading data file
 def readFile(fname):
+    global rows  # test
+    rows = []
     f = open(fname, 'rb')
     reader = csv.reader(f, delimiter='\t')
     for n, row in enumerate(reader):
@@ -25,6 +27,8 @@ def readFile(fname):
             rows.append([])
             rows[n].extend(row)
     f.close()
+    return rows
+
 
 # reading batches of files
 def readFiles():
@@ -32,22 +36,22 @@ def readFiles():
         print "processing a directory"
         list_of_files = glob.glob('%s/%s*' % (datapath, filemask))
         list_of_files.sort()  # need to sort it in different way!
+        print "Number of files in directory: %d \n" % len(list_of_files)
     else:
         print "processing a list of files"
         #list_of_files = sys.argv[1:]
 
-    # i = 0
-    y = numpy.array([])
+    # y = numpy.array([])
     for file_name in list_of_files:
         print file_name
         readFile(file_name)
         data = numpy.array(rows, float)
         print data
-        # y = numpy.append(y, data, )
-        # x.append(data)
-        # x[i].extend(data)
-        # i += 1
 
-    print y
 
+# readFile(datapath + '/scan_down_0')
+# x = numpy.array(rows, float)
+# readFile(datapath + '/scan_down_1')
+# y = numpy.array(rows, float)
+# print x + y
 readFiles()
